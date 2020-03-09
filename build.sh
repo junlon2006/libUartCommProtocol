e5e984fc4e9edfbd21b7ebce3ff0e4175344c301
shangjinlong <shangjinlong@unisound.com>
2020-03-09 11:58:06 +0800

    :construction: add benchmark images

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-g++ -std=c++11 -Wall -Werror -O2 -o peera src/uni_communication.c utils/uni_crc16.c utils/uni_log.c utils/uni_interruptable.c demo/peer_a.c -Iinc -Iutils -lpthread
-g++ -std=c++11 -Wall -Werror -O2 -o peerb src/uni_communication.c utils/uni_crc16.c utils/uni_log.c utils/uni_interruptable.c demo/peer_b.c -Iinc -Iutils -lpthread
+g++ -std=c++11 -Wall -Werror -O2 -o peera src/uni_communication.c utils/uni_crc16.c utils/uni_log.c utils/uni_interruptable.c benchmark/peer_a.c -Iinc -Iutils -lpthread
+g++ -std=c++11 -Wall -Werror -O2 -o peerb src/uni_communication.c utils/uni_crc16.c utils/uni_log.c utils/uni_interruptable.c benchmark/peer_b.c -Iinc -Iutils -lpthread
